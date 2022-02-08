92585eba54038f2ab45f0a43870498b109908abd
dannnnnnnnnn10 <97569221+dannnnnnnnnn10@users.noreply.github.com>
2022-02-08 18:52:16 -0500

    my take at bishop logic

---
+++ Bishop.java
@@ -0,0 +1,65 @@
+package Project2;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Player player) {
+		super(player);
+	}
+
+	public String type() {
+		return "Bishop";
+	}
+
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
+		boolean valid = super.isValidMove(move, board);
+		if(valid == false){
+			return valid;
+		}
+
+		if(Math.abs(move.fromRow - move.toRow)
+				!= Math.abs(move.fromColumn - move.toColumn)) {
+			return false;
+		}
+
+		//Checks if bishop is moving up left
+		if(move.fromRow - move.toRow < 0 &&
+				move.fromColumn - move.toColumn > 0){
+			for(int i = 1; i < Math.abs(move.fromRow - move.toRow); i++){
+				if(board[move.fromRow + i][move.fromColumn - i] != null){
+					valid = false;
+				}
+			}
+		}
+		//Checks if bishop is move up right
+		if(move.fromRow - move.toRow > 0 &&
+				move.fromColumn - move.toColumn > 0){
+			for(int i = 1; i < Math.abs(move.fromRow - move.toRow); ++i){
+				if(board[move.fromRow - i][move.fromColumn - i] != null){
+					valid = false;
+				}
+			}
+		}
+		//Checks if bishop is moving down left
+		if(move.fromRow - move.toRow < 0 &&
+				move.fromColumn - move.toColumn < 0){
+			for(int i = 1; i < Math.abs(move.fromRow - move.toRow); i++){
+				if(board[move.fromRow + i][move.fromColumn + i] != null){
+					valid = false;
+				}
+			}
+		}
+
+		//Bishop is moving down right
+		if(move.fromRow - move.toRow > 0 &&
+				move.fromColumn - move.toColumn < 0){
+			for(int i = 1; i < Math.abs(move.fromRow - move.toRow); i++){
+				if(board[move.fromRow - i][move.fromColumn + i] != null){
+					valid = false;
+				}
+			}
+		}
+
+		return valid;
+
+	}
+}
