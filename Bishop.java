3c7e8239821035744a3c35c6af5e5bf03b26d5c7
kyle512 <98427493+kyle512@users.noreply.github.com>
2022-02-10 15:27:07 -0500

    Fixed

--- Bishop.java
+++ Bishop.java
@@ -17,72 +17,47 @@ public class Bishop extends ChessPiece {
         }
 
         //Bishop can only move diagonally unlimited spaces
-        //meaning each move should increase the row and column
-        if (Math.abs(move.fromRow - move.toRow) < 1 ||
-                Math.abs(move.fromColumn - move.toColumn) < 1) {
-            valid = false;
+        //meaning each move should increase the row and column equally
+        if(Math.abs(move.fromRow - move.toRow) !=
+                Math.abs(move.fromColumn - move.toColumn)){
+            return false;
         }
-        //Checks if bishop is moving forward left
-        if(move.fromRow - move.toRow > 0 &&
-                move.fromColumn - move.toColumn >0){
-            for(int i = move.fromRow - 1; i > move.toRow; i--){
-                for(int k = move.fromColumn - 1; k > move.toColumn; k--){
-                    if(board[i][k] != null){
-                        valid = false;
-                        break;
-                    }
-                    else{
-                        break;
-                    }
-                }
-            }
-        }
-        //Checks if bishop is move forward right
-        if(move.fromRow - move.toRow > 0 &&
-                move.toColumn - move.fromColumn >0){
-            for(int i = move.fromRow - 1; i > move.toRow; i--){
-                for(int k = move.fromColumn + 1; k < move.toColumn; k++){
-                    if(board[i][k] != null){
-                        valid = false;
-                        break;
-                    }
-                    else{
-                        break;
-                    }
-                }
-            }
-        }
-        //Checks if bishop is moving back left
-        if(move.toRow - move.fromRow > 0 &&
+        //Checks if bishop is moving up left
+        if(move.fromRow - move.toRow < 0 &&
                 move.fromColumn - move.toColumn > 0){
-            for(int i = move.fromRow + 1; i < move.toRow; i++){
-                for(int k = move.fromColumn - 1; k > move.toColumn; k--){
-                    if(board[i][k] != null){
-                        valid = false;
-                        break;
-                    }
-                    else{
-                        break;
-                    }
+            for(int i = 1; i < Math.abs(move.fromRow - move.toRow); i++){
+                if(board[move.fromRow + i][move.fromColumn - i] != null){
+                    valid = false;
                 }
             }
         }
-        //Bishop is moving back right
-        if(move.toRow - move.fromRow > 0 &&
-                move.toColumn - move.fromColumn > 0){
-            for(int i = move.fromRow + 1; i < move.toRow; i++){
-                for(int k = move.fromColumn + 1; k < move.toColumn; k++){
-                    if(board[i][k] != null){
-                        valid = false;
-                        break;
-                    }
-                    else{
-                        break;
-                    }
+        //Checks if bishop is move up right
+        if(move.fromRow - move.toRow > 0 &&
+                move.fromColumn - move.toColumn > 0){
+            for(int i = 1; i < Math.abs(move.fromRow - move.toRow); ++i){
+                if(board[move.fromRow - i][move.fromColumn - i] != null){
+                    valid = false;
                 }
             }
         }
-
+        //Checks if bishop is moving down left
+        if(move.fromRow - move.toRow < 0 &&
+                move.fromColumn - move.toColumn < 0){
+            for(int i = 1; i < Math.abs(move.fromRow - move.toRow); i++){
+                if(board[move.fromRow + i][move.fromColumn + i] != null){
+                    valid = false;
+                }
+            }
+        }
+        //Bishop is moving down right
+        if(move.fromRow - move.toRow > 0 &&
+                move.fromColumn - move.toColumn < 0){
+            for(int i = 1; i < Math.abs(move.fromRow - move.toRow); i++){
+                if(board[move.fromRow - i][move.fromColumn + i] != null){
+                    valid = false;
+                }
+            }
+        }
         return valid;
 
     }
